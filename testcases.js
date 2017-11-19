54ba3a9e3ad1d79378a8654395f55e12e2501ad2
Quasic <qbasic010@aim.com>
2017-11-19 02:50:30 -0500

    More testcases, simplify Travis CI config

--- testcases.js
+++ testcases.js
@@ -1,11 +1,17 @@
+// Testcases to be run in node.js, WScript (preferrably the CScript variant) or a page in a browser
 var
-	Tileset = [
-		"",
-		"",
-		"",
-		""
-	],
+	DirTile = "t",
+	DirChar = "c",
+	DirItem = "i",
+	DirBuild = "b",
+	MapSizeX = 13,
+	MapSizeY = 7,
+	MapSizeX1 = MapSizeX + 1,
+	MapSizeY1 = MapSizeY + 1,
+	TotalTiles = MapSizeX1 * MapSizeY1,
+	Tileset = (function(){var r="",i,m=["Gg","Ge","Gb","Gd","Og","Re"];for(i=0;i<TotalTiles*4;i++){r+=m[Math.floor(Math.random()*100)]||"Ga";}return[r.substring(0,TotalTiles),r.substring(TotalTiles,TotalTiles*2),r.substring(TotalTiles*2,TotalTiles*3),r.substring(TotalTiles*3,TotalTiles*4)];})(),
 	ItemID,
+	cache={t:{},i:{},b:{},c:{}},
 	includes=[
 		"client-startscreen.js",
 		"client-itemid.js"];
@@ -13,7 +19,19 @@ function testcases(assert,log){
 	function warn(m){
 		log("Warning: "+m);
 	}
-	log("Testing Tileset");
+	function assertTilesExist(s,t){
+		var f;
+		for(var j = 0; j < s.length; j+=2)assert(cache.t[f=(s.substring(j,j+2))+".png"],"Tile file not found: "+DirTile+"/"+f+" for "+t+" @ tile "+j/2+" / "+s.length/2+", char "+j+" / "+s.length);
+	}
+	log("Testing tiles");
+	assertTilesExist(
+		"Ga"
+		, "[basic Tiles: Ga etc.]"
+	);
+	
+	var r=/[A-Z][a-z].png/;
+	for(var i in cache.t)if(cache.t.hasOwnProperty(i)&&i.search(r)<0)warn(DirTile+"/"+i+" is not a normal tile filename");
+	log("Testing startscreen");
 	log(Tileset);
 	assert(Tileset && Object.prototype.toString.apply(Tileset) === "[object Array]","Tileset clobbered.");
 	if(Tileset.length !== 4)
@@ -21,25 +39,76 @@ function testcases(assert,log){
 	for(var i = 0; i < 4; i++){
 		assert(typeof Tileset[i] === "string", "Tileset["+i+"] not a string");
 		assert(Tileset[i].search(/^([A-Z][a-z])*$/) >= 0, "Tileset["+i+"] bad format");
-		//if(Tileset[i].length !== )warn("Tileset["+i+"] length is "+Tileset[i].length+" instead of "+);
-		// for(var j = 0; j < Tileset[i].length; j++)test for each image existance
+		if(Tileset[i].length !== TotalTiles)warn("Tileset["+i+"] length is "+Tileset[i].length+" instead of "+TotalTiles);
+			assertTilesExist(Tileset[i], "Tileset["+i+"]");
+		}
+	log("Testing Characters");
+	//starting images CHRHne... CHRnw...
+	var r=/^CH(I[A-Z][a-z]|RH[MF][0-9]|R[MF][0-9]|RHne|Rnw)([LR]|)\.gif$/,m,d;
+	for(var i in cache.c)if(cache.c.hasOwnProperty(i)){
+		m=i.match(r);
+		if(m){
+			if(m[1].charAt(0) === "I" && !cache.i["O"+m[1].substring(1)+".gif"])warn("Item '"+m[1].substring(1)+"', "+ItemID(m[1].substring(1))+", has a wearable image, "+DirChar+"/"+i+", but no matching item image "+DirItem+"/"+"O"+m[1].substring(1)+".gif");
+			if(m[2]){
+				d={L:"R",R:"L"}[m[2]];
+				assert(cache.c["CH"+m[1]+d+".gif"],"There is a "+DirChar+"/"+i+", but no matching "+DirChar+"/"+"CH"+m[1]+d+".gif");
+			}
+		}else{
+			warn(i+" is not a normal character image name");
+		}
 	}
-	log("Testing ItemID");
+	log("Testing ItemID, items");
+	function hasItemID(id){
+		var i=ItemID(id);
+		return i && (i.substring(0,4) !== "Item" || i.substring(i.length-id.length) !== id);
+	}
 	assert(ItemID, "ItemID "+ typeof ItemID);
-	//...
+	r=/^O[A-Z][a-z]\.gif$/;
+	for(var i in cache.i)if(cache.i.hasOwnProperty(i)){
+		if(i.search(r)<0)
+			warn(DirItem+"/"+i+" is not a normal item filename");
+		else if(!hasItemID(i.substring(1,3)))
+			warn("ItemID '"+i.substring(1,3)+"' not defined, but "+DirItem+"/"+i+" exists");
+	}
+	for(var i="A"; i<"Z"; i=String.fromCharCode(i.charCodeAt(0)+1))
+		for(var j="a"; j<"z"; j=String.fromCharCode(j.charCodeAt(0)+1))
+			if(!cache.i["O"+i+j+".gif"]&&hasItemID(i+j))
+				warn("Item '"+i+j+"', "+ItemID(i+j)+", has no image, "+DirItem+"/O"+i+j+".gif");
+	log("Testing ItemID, buildings");
+	r=/^[A-Z]+\.gif$/;
+	for(var i in cache.b)if(cache.b.hasOwnProperty(i)){
+		if(i.search(r)<0)
+			warn(DirBuild+"/"+i+" is not a normal building filename");
+		else if(!hasItemID(i.substring(0,i.length-4)))
+			warn("ItemID '"+i.substring(0,i.length-4)+"' not defined, but "+DirItem+"/"+i+" exists");
+	}
+	log("Testing ItemID, NPCs");
+	// how should this work?
 	log("Tests finished. No critical failures found.");
 }
+
+// Bootstrap:
+
 if(typeof WScript == "object"){
 	WScript.echo("WScript "+WScript.version);
 	var
 		fso=new ActiveXObject("Scripting.FileSystemObject"),
 		h,
 		i;
+	WScript.echo("Loading scripts...");
 	for(i=0;i<includes.length;i++){
 		h=fso.openTextFile(includes[i],1);
 		eval(h.readAll());
 		h.close();
 	}
+	WScript.echo("Listing images...");
+	function readdir(d,k){
+		for(i=new Enumerator(fso.getFolder(d).files); i.hasNext(); i.moveNext())cache[k][i.item().name]=1;
+	}
+	readdir(DirTile,"t");
+	readdir(DirBuild,"b");
+	readdir(DirItem,"i");
+	readdir(DirChar,"c");
 	testcases(
 		function(t,m){if(!t)throw new Error(m);},
 		function(m){WScript.echo(m);}
@@ -50,7 +119,14 @@ if(typeof WScript == "object"){
 		fs = require("fs"),
 		vm = require("vm"),
 		assert = require("assert"),
-		sandbox = {Tileset: Tileset };
+		d,
+		sandbox = {
+			MapSizeX: MapSizeX,
+			MapSizeY: MapSizeY,
+			MapSizeX1: MapSizeX1,
+			MapSizeY1: MapSizeY1,
+			Tileset: Tileset
+		};
 	vm.createContext(sandbox);
 	function tagsrc(src,callback){
 		console.log(src);
@@ -73,19 +149,38 @@ if(typeof WScript == "object"){
 		}
 		f();
 	}
-	console.log("Loading script tags...");
+	console.log("Loading scripts...");
 	scriptags(
 		includes,
 		function(){
-			console.log("Script tags loaded, starting tests...");
-			ItemID=sandbox.ItemID;
-			testcases(assert,console.log);
+			console.log("Listing images...");
+			d="i";
+			fs.readdir(DirItem,cacheFiles);
 		}
 	);
+	function cacheFiles(err,files){
+		for(var i=0; i<files.length; i++)cache[d][files[i]]=1;
+		d={i:"t", t:"b", b:"c"}[d];
+		if(d)fs.readdir({
+			t:DirTile,
+			b:DirBuild,
+			c:DirChar}[d],
+			cacheFiles
+		);
+		else start();
+	}
+	function start(){
+		console.log("Script tags loaded, starting tests...");
+		ItemID=sandbox.ItemID;
+		testcases(
+			assert,
+			console.log
+		);
+	}
 }else if(typeof window === "object"){
 	// assuming script tags present in correct order
 	function l(m){
-		// TODO: figure out log implementation
+		// TODO: figure out log and fexists implementations
 	}
 	window.onerror=function(u,m,l){
 		l("Error in "+u+"<"+l+">: "+m);
@@ -93,6 +188,7 @@ if(typeof WScript == "object"){
 	l(window.navigator);
 	testcases(
 		function(t,m){if(!t)throw new Error(m);},
-		l
+		l,
+		fexists
 	);
 }
